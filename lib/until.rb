03bc544fe1e48549a523904b69be2dfceee291db
Yueh Han <yuehwhan@gmail.com>
2019-12-15 02:09:22 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
   levitation_force = 6
   until levitation_force == 10
     puts "Wingardium Levios"
-    levitation_force += 1
+    levitation_force = levitation_force + 1
   end
 
 end
